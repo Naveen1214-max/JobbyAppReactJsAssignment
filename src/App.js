956da78b7e37981becb2f1e7902806db4c6cbd8d
naveen <naveenkumarbasupally@gmail.com>
2023-03-25 05:56:38 +0000

    testcases are passed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
-import JobItemDetails from './components/JobItemDetails'
-import Jobs from './components/Jobs'
+import {Switch, Route} from 'react-router-dom'
+
+import LoginSection from './Components/LoginSection'
+import Home from './Components/Home'
+import ProtectedRoute from './Components/ProtectedRoute'
+import Jobs from './Components/Jobs'
+import NotFound from './Components/NotFound'
+import JobDetails from './Components/JobDetails'
 import './App.css'
 
 const App = () => (
   <Switch>
-    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/login" component={LoginSection} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/jobs" component={Jobs} />
-    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
+    <ProtectedRoute exact path="/jobs/:id" component={JobDetails} />
+    <Route component={NotFound} />
   </Switch>
 )
 
